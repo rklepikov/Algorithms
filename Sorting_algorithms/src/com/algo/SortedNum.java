81286651d825d99bfd4abc1754ee0cc5c32c40bc
Roman Klepikov <r.g.klepikov@gmail.com>
2021-05-04 19:22:34 +0500

    add method bubbleSort

--- Sorting_algorithms/src/com/algo/SortedNum.java
+++ Sorting_algorithms/src/com/algo/SortedNum.java
@@ -1,5 +1,23 @@
 package com.algo;
 
+import java.util.Arrays;
+
 public class SortedNum {
-    
+    public static void bubbleSort(int[] array){
+        for(int i = array.length; i > 0; i--){
+            for(int j = 1; j < i; j++){
+                if(array[j] < array[j-1]){
+                    int temp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] num = new int[] {12, 3, 43, 10, 1, -23, 90, 13};
+        bubbleSort(num);
+        System.out.println(Arrays.toString(num));
+    }
 }
